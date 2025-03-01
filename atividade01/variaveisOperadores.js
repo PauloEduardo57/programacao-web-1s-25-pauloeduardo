5c15b19da84fcf0c3cda0e90746bba650d043a1e
PauloEduardo57 <paulo.barbosa@a.ucb.br>
2025-03-01 17:02:10 -0300

    envio Atividade 1 - Variáveis e tipos primitivos

---
+++ atividade01/variaveisOperadores.js
@@ -0,0 +1,23 @@
+let numA1 = 10, numB1 = 5;
+let resultadoSoma = numA1 + numB1;
+console.log(`Resultado da soma entre ${numA1} e ${numB1} é ${resultadoSoma}`);
+
+
+let numA2 = 20, numB2 = 8;
+let resultadoSubtracao = numA2 - numB2;
+console.log(`Resultado da subtração entre ${numA2} e ${numB2} é ${resultadoSubtracao}`);
+
+
+let numA3 = 7, numB3 = 3;
+let resultadoMultiplicacao = numA3 * numB3;
+console.log(`Resultado da multiplicação entre ${numA3} e ${numB3} é ${resultadoMultiplicacao}`);
+
+
+let numA4 = 15, numB4 = 3;
+let resultadoDivisao = numA4 / numB4;
+console.log(`Resultado da divisão entre ${numA4} e ${numB4} é ${resultadoDivisao}`);
+
+
+let numA5 = 10, numB5 = 4;
+let resultadoModulo = numA5 % numB5;
+console.log(`Resultado do módulo entre ${numA5} e ${numB5} é ${resultadoModulo}`);
