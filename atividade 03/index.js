955e753956d2bab92213711328633597b9b6f986
PauloEduardo57 <paulo.barbosa@a.ucb.br>
2025-03-25 13:00:10 -0300

    atividade 03

---
+++ atividade 03/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const calculadora = require('./util/calculadora');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/somar/:a/:b', (req, res) => {
+    const { a, b } = req.params;
+    res.json({ resultado: calculadora.somar(Number(a), Number(b)) });
+});
+
+app.get('/subtrair/:a/:b', (req, res) => {
+    const { a, b } = req.params;
+    res.json({ resultado: calculadora.subtrair(Number(a), Number(b)) });
+});
+
+app.get('/multiplicar/:a/:b', (req, res) => {
+    const { a, b } = req.params;
+    res.json({ resultado: calculadora.multiplicar(Number(a), Number(b)) });
+});
+
+app.get('/dividir/:a/:b', (req, res) => {
+    const { a, b } = req.params;
+    
+    if (Number(b) === 0) {
+        return res.status(400).json({ erro: "Divisão por zero não permitida." });
+    }
+
+    res.json({ resultado: calculadora.dividir(Number(a), Number(b)) });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
